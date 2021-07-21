50b9e69564db1e6bf1248cf872bca78f9fe22f4b
David V. McKay <david.v.mckay@gmail.com>
2021-07-21 18:15:58 -0400

    wip - prod build

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,23 +3,26 @@ import { VitePluginNode } from 'vite-plugin-node';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
 
-export default defineConfig({
-  // server: { port: 3000 },
-  plugins: [
-    reactJsx(),
-    reactRefresh(),
-    ...VitePluginNode({
-      server: 'express',
-      appPath: './server.ts',
-      port: 3000,
-      tsCompiler: 'esbuild',
-      // createCustomServer: () => 
-    })
-  ],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './server.ts',
+export default async (args: {command: 'serve' | 'build' | string, mode: 'development' | string} & {[K in string]: unknown}) => {
+  console.log(args);
+  return defineConfig({
+    // server: { port: 3000 },
+    plugins: [
+      reactJsx(),
+      reactRefresh(),
+      !process.argv0.includes('vite-node') ? [] : VitePluginNode({
+        server: 'express',
+        appPath: './server.ts',
+        port: 3000,
+        tsCompiler: 'esbuild',
+        // createCustomServer: () => 
+      })
+    ],
+    build: {
+      manifest: true,
+      rollupOptions: {
+        input: './server.ts',
+      },
     },
-  },
-});
+  });
+};
